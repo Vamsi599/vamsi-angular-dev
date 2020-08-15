6893a102cb5211d648526cae15f5114ef3f59608
Vamsi599 <vamsi.jonnal@gmail.com>
2020-08-15 11:48:04 +0530

    1 file modified, 1 added, 1 deleted

--- src/app/authentication/authentication.module.ts
+++ src/app/authentication/authentication.module.ts
@@ -3,9 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 
 import { FlexLayoutModule } from '@angular/flex-layout';
-import { AuthenticationRoutingModule } from 'authentication-routing.module';
+import { AuthenticationRoutingModule } from './authentication-routing.module';
+import { AuthenticationComponent } from './authentication.component';
 
 @NgModule({
+  declarations:[AuthenticationComponent],
   imports:      [ BrowserModule, FormsModule , FlexLayoutModule, AuthenticationRoutingModule ],
 
 })
